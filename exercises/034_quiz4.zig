c7c39c482e0ff600bc2a9f51cab47cd156d4f5aa
MevyDev <mevydev@proton.me>
2025-11-07 11:26:04 +0100

    034 comment out expanding lines

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -13,15 +13,15 @@ pub fn main() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
 
     const my_num1: u32 = try getNumber();
-    const my_num2: u32 = getNumber() catch 0;
-    const my_num3: u32 = getNumber() catch {
-        std.debug.print("oh noooo :(\n", .{});
-        return;
-    };
+    // const my_num2: u32 = getNumber() catch 0;
+    // const my_num3: u32 = getNumber() catch {
+    //     std.debug.print("oh noooo :(\n", .{});
+    //     return;
+    // };
 
     try stdout.interface.print("my_num={}\n", .{my_num1});
-    try stdout.interface.print("my_num={}\n", .{my_num2});
-    try stdout.interface.print("my_num={}\n", .{my_num3});
+    // try stdout.interface.print("my_num={}\n", .{my_num2});
+    // try stdout.interface.print("my_num={}\n", .{my_num3});
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
