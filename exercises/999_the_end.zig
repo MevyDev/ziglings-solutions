67a77a376e87b09f10bc1e9720b2f02fbf663e0f
MevyDev <mevydev@proton.me>
2025-11-19 23:07:08 +0100

    love zig and ziglings <3

--- exercises/999_the_end.zig
+++ exercises/999_the_end.zig
@@ -5,4 +5,5 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     print("\nThis is the end for now!\nWe hope you had fun and were able to learn a lot, so visit us again when the next exercises are available.\n", .{});
+    // this was so cool love you ziglings
 }
